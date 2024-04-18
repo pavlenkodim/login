1db6804b55792570ebfadc5142fcfa679521555f
Dmitry <pavlenkodim@mail.ru>
2024-04-19 00:23:35 +0500

    Add function showPassword

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('hi');
+// Main functions
+
+function showPassword () {
+    
+    const input = document.querySelector('#password');
+    const checkbox = document.querySelector('#showpass');
+    
+    if (checkbox.checked) {
+        input.type = 'text';
+    } else {
+        input.type = 'password';
+    }
+}
